2656a2ffc7a4f8b9551c67741b3b1f9ef85f0dcd
unknown <713priyanshusingh@gmail.com>
2023-01-30 20:46:16 +0530

    InterestCalc updated

--- src/interestCalc.js
+++ src/interestCalc.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { ReactDOM } from 'react';
+import Slider from '@mui/material/Slider';
+import { makeStyles } from '@mui/material/styles';
+
+
 function InterestCalc()
 {
 
@@ -31,12 +35,28 @@ useEffect(()=>{
              <input type="number" value={num1 || ""} onChange={(e)=>setNum1(e.target.value)} placeholder="$0" 
              className="Principal-Amount"/><br/>
               <label>Year</label>
-                   <input type="number" value={num2 || ""} placeholder="0" onChange={(e)=>setNum2(e.target.value)}
-                      className="Year"></input><br/>
-              <label>Rate</label>
-                  <input type="number" value={num3 || ""} placeholder="0" onChange={(e)=>setNum3(e.target.value)}
-                    className="Rate"></input><br/>
-            
+         <div className="year"><Slider style={{ height: 5, width: 150 }}
+ value={num2 || ""} onChange={(e)=>setNum2(e.target.value)}
+            defaultValue={1}
+            aria-labelledby="discrete-slider"
+            valueLabelDisplay="auto"
+            step={1}
+            min={1}
+            max={10}
+         />
+        </div>
+              <label>Rate</label><br></br>
+        <Slider style={{ height: 5, width: 150 }}
+         value={num3 || ""} onChange={(e)=>setNum3(e.target.value)}
+            defaultValue={5}
+            aria-labelledby="discrete-slider"
+            valueLabelDisplay="auto"
+            step={1}
+            min={5}
+            max={15}
+
+         />
+
      </div>
      </form>
      </div>
